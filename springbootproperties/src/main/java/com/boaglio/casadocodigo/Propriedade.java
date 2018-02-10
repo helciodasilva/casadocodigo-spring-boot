038f9a9233c1d97867d33bc719a2b8195bce1ab0
Helcio da Silva <helciodasilva@gmail.com>
2018-02-10 08:13:41 -0200

    Capítulo 3

---
+++ springbootproperties/src/main/java/com/boaglio/casadocodigo/Propriedade.java
@@ -0,0 +1,56 @@
+package com.boaglio.casadocodigo;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+public class Propriedade {
+
+	@Id
+	private String nome;
+	private String valor;
+	private String descricao;
+	private String categoria;
+	private String subcategoria;
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getValor() {
+		return valor;
+	}
+
+	public void setValor(String valor) {
+		this.valor = valor;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	public String getCategoria() {
+		return categoria;
+	}
+
+	public void setCategoria(String categoria) {
+		this.categoria = categoria;
+	}
+
+	public String getSubcategoria() {
+		return subcategoria;
+	}
+
+	public void setSubcategoria(String subcategoria) {
+		this.subcategoria = subcategoria;
+	}
+
+}
